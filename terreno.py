1b86f871d6549c884ef41145acde192d8ace13d4
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-02 03:45:15 -0300

    Delete terreno.py

--- terreno.py
+++
@@ -1,4 +0,0 @@
-largura = int(input("digte a largura em metros:"))
-profundidade = int(input("digite a profundidade em metros:"))
-area = largura * profundidade
-print(f"o terreno tem {area//10000} hectares")
