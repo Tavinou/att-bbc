433c53e08492d119157f91e8d17dcca4b078648b
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:20:35 -0300

    Delete GHR-Alg-01-Ex-08.py

--- GHR-Alg-01-Ex-08.py
+++
@@ -1,8 +0,0 @@
-bugigangas = int(input("Digte a quantidade de bugigangas:"))
-quinquilharias = int(input("Digite a quantidade de quinquilharias:"))
-
-bugigangas = bugigangas * 75
-quinquilharias = quinquilharias * 112
-valortotal = bugigangas + quinquilharias
-
-print(f"o peso total é de {valortotal}g")
