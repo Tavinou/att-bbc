cb659bfbad9ff2981f0cb665726f13032942f5ef
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:22:52 -0300

    Delete GHR-Alg-01-Ex-14.py

--- GHR-Alg-01-Ex-14.py
+++
@@ -1,9 +0,0 @@
-import math
-lado1 = float(input("lado 1:"))
-lado2 = float(input("lado 2:"))
-lado3 = float(input("lado 3:"))
-
-l = (lado1+lado2+lado3) / 2
-
-area = math.sqrt(l*((l-lado1)*(l-lado2)*(l-lado3)))
-print(f"area do triângulo:{area:.2f}")
