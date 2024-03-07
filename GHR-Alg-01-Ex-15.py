f5e7e008df828b2cfdb96de2ea7f4c20eec4fd00
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:20:42 -0300

    Delete GHR-Alg-01-Ex-15.py

--- GHR-Alg-01-Ex-15.py
+++
@@ -1,7 +0,0 @@
-import math
-lado = float(input("comprimento do lado:"))
-num = int(input("número de lados:"))
-
-area = (num* lado**2)/(4*math.tan(math.pi/num))
-
-print(f"A area do polígono regular{area:.2f}")
