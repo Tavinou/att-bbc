53df5c26d5bc62c2d2cd530fbafb77a620e17a0f
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:20:03 -0300

    Delete GHR-Alg-01-Ex-04.py

--- GHR-Alg-01-Ex-04.py
+++
@@ -1,4 +0,0 @@
-largura = float(input("digte a largura em metros:"))
-profundidade = float(input("digite a profundidade em metros:"))
-area = largura * profundidade
-print(f"o terreno tem {area//10000} hectares")
