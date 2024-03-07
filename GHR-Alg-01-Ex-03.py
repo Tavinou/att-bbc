0f6e6df4484b059be1071b0c9ccb970ca2a4d79a
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:19:54 -0300

    Delete GHR-Alg-01-Ex-03.py

--- GHR-Alg-01-Ex-03.py
+++
@@ -1,4 +0,0 @@
-largura = int(input("digte a largura em metros:"))
-profundidade = int(input("digite a profundidade em metros:"))
-area = largura * profundidade
-print(f"a area da sala é: {area}m²")
