aece088babe868aacc0e447aa9870968f4f7ad93
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-07 11:14:01 -0300

    Delete Lista 2 directory

--- Lista 2/GHR-Alg-02-Ex-04.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input("digite um numero:"))
-n2 = int(input("digite um numero:"))
-n3 = int(input("digite um numero:"))
-
-print(f"max{max(n1,n2,n3)}")
-print(f"min{min(n1,n2,n3)}")
-print(f"meio{(n1+n2+n3)-(max(n1,n2,n3)+min(n1,n2,n3))}")
