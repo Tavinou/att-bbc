fb80f4feb5e8e77acaee10c64f202dcc0b425888
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:19:16 -0300

    a
    a

---
+++ Lista 1/GHR-Alg-01-Ex-03.py
@@ -0,0 +1,4 @@
+largura = int(input("digte a largura em metros:"))
+profundidade = int(input("digite a profundidade em metros:"))
+area = largura * profundidade
+print(f"a area da sala é: {area}m²")
