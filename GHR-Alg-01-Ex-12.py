d118d827151889f8a8d1730f8c779ef98bfca64c
Tavinou <104601052+Tavinou@users.noreply.github.com>
2024-03-06 21:22:23 -0300

    Delete GHR-Alg-01-Ex-12.py

--- GHR-Alg-01-Ex-12.py
+++
@@ -1,9 +0,0 @@
-import math
-unidade = input("digite unidade de medida (ex m):")
-raio = float(input("digite o raio:"))
-
-area = math.pi * raio**2
-volume = (4/3) * math.pi * raio**3
-
-print(f"area do circulo é {area:.2f}{unidade}²")
-print(f"area do circulo é {volume:.2f}{unidade}³")
