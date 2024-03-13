f244bc1e181c31b4f255a169c6bb210fa2b2bc9f
leleHenscel <95099159+leleHenscel@users.noreply.github.com>
2024-03-13 15:35:10 -0300

    Update GHR-Alg-02-Ex-10.py

--- Lista 2/GHR-Alg-02-Ex-10.py
+++ Lista 2/GHR-Alg-02-Ex-10.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 mat = (int(input("digite a matricula:")))
 a = mat//10000
 s = (mat%10000)//1000
 
 print(f"ano: {a} semestre:{s}")
-=======
 mat = (int(input("digite a matricula:")))
->>>>>>> ce58a64bc5cccad4fb228b3c9f623a9eff247397
